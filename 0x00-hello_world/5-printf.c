eb79d3631e1ead4d445f3e6f762ae04343d47801
amjonas <amoquandohjonas@gmail.com>
2022-06-30 00:51:15 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/*A simple Program that prints excatly with proper grammar, but the outcome is a piece of art,*/
+
+#include <stdio.h>
+
+    int main(void)
+
+{
+       printf("with proper grammar, but the outcome is a piece of art,\n");
+         return (0);
+}
