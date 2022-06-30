599b67e8e43ba5b6b7afcaf45a1f250943663a6e
amjonas <amoquandohjonas@gmail.com>
2022-06-30 00:06:34 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*main function - where the execution of program begins*/
+	int main(void)
+
+		{
+
+			/*prints text in quotes*/
+		puts("Programming is like building a multilingual puzzle");
+		return (0);
+
+		}
