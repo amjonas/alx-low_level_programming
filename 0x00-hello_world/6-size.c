37cbc4c9a0c3ffa6090fe334825a99fc5c376ba0
amjonas <amoquandohjonas@gmail.com>
2022-06-30 21:27:42 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,10 @@
-/*A C program that prints the size of various types on the computer it is compiled and run on*/
-
 #include <stdio.h>
+
+/**
+ * main - Prints the size of various types on the computer it is compiled and run on.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
@@ -10,5 +14,6 @@ int main(void)
     printf("Size of a long int: %d byte(s)\n", sizeof(long int));
     printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
     printf("Size of a float: %d byte(s)\n", sizeof(float));
+
     return (0);
 }
