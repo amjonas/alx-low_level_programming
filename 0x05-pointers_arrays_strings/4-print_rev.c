9ef9874ad800f47c21252baf4d472b3205a26f4e
amjonas <amoquandohjonas@gmail.com>
2022-07-11 23:17:12 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *  print_rev -> printing a string in reverse
+ *  @s: the string to be printed in rev
+ *  Return: void
+ */
+
+void print_rev(char *s)
+
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
